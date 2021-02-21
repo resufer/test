ad700591ecd73b1c1c0e7daaa15ceee0da6233f8
resufer <66846932+resufer@users.noreply.github.com>
2021-02-21 16:08:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+  let x = document.getElementById('x')
+  let y = document.getElementById('y')
+  document.body.onmousemove = (e) => {
+    x.innerText = 'x:' + e.clientX;
+    y.innerText = 'y:' + e.clientY;
+  };
+
+
+  let startCanvas = function (e) {
+    let canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    ctx.moveTo(e.clientX, e.clientY - 30);
+    ctx.strokeStyle = 'black';
+    canvas.onmousemove = (e) => {
+      ctx.lineTo(e.clientX, e.clientY - 30);
+      ctx.stroke();
+    }
+    document.body.onkeypress = (e) => {
+      if (e.keyCode === 32) {
+        ctx.globalCompositeOperation = 'destination-out';
+      }
+    }
+  }
+  document.body.onclick = (e) => startCanvas(e);
+};
